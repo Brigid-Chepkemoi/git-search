2744e9ef45440d98bdaba0e9a5859cb106b9c889
brie <briememo95@gmail.com>
2022-04-18 23:03:55 +0300

    add form component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
+import { GitSearchFormComponent } from './git-search-form/git-search-form.component';
+import { GitsearchFormComponent } from './gitsearch-form/gitsearch-form.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
+    GitSearchFormComponent,
+    GitsearchFormComponent,
  
   ],
   imports: [
