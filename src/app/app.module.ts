b9befe721de168fd27d48fea5bfc3ebcf070f803
brie <briememo95@gmail.com>
2022-04-18 23:21:23 +0300

    add repo component and template styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
 import { GitsearchFormComponent } from './gitsearch-form/gitsearch-form.component';
+import { RepoComponent } from './repo/repo.component';
 
 
 @NgModule({
@@ -12,6 +13,7 @@ import { GitsearchFormComponent } from './gitsearch-form/gitsearch-form.componen
     AppComponent,
     NavComponent,
     GitsearchFormComponent,
+    RepoComponent,
  
   ],
   imports: [
